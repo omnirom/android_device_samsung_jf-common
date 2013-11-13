7cbf028d605f5451c53862d6e482bd46fae63ea0
Dan Pasanen <dan.pasanen@gmail.com>
2013-11-12 19:53:14 -0600

    jf: update sepolicy
    Change-Id: I3e8fa10e12a30d61b57b46bc1ba7c66a695fb4d7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,35 +104,34 @@ TW_BRIGHTNESS_PATH := /sys/devices/platform/msm_fb.526593/leds/lcd-backlight/bri
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
 
 # SELinux
-#BOARD_SEPOLICY_DIRS += \
-#        device/samsung/jf-common/sepolicy
-#
-#BOARD_SEPOLICY_UNION += \
-#	file_contexts \
-#	property_contexts \
-#	te_macros \
-#	bluetooth_loader.te \
-#	bridge.te \
-#	camera.te \
-#	conn_init.te \
-#	device.te \
-#	dhcp.te \
-#	domain.te \
-#	drmserver.te \
-#	file.te \
-#	init.te \
-#	kickstart.te \
-#	mediaserver.te \
-#	mpdecision.te \
-#	netmgrd.te \
-#	property.te \
-#	qmux.te \
-#	rild.te \
-#	rmt.te \
-#	sensors.te \
-#	surfaceflinger.te \
-#	system.te \
-#	tee.te \
-#	thermald.te \
-#	ueventd.te \
-#	wpa_supplicant.te
+BOARD_SEPOLICY_DIRS += \
+        device/samsung/jf-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+	file_contexts \
+	property_contexts \
+	te_macros \
+	bluetooth_loader.te \
+	bridge.te \
+	camera.te \
+	conn_init.te \
+	device.te \
+	dhcp.te \
+	domain.te \
+	drmserver.te \
+	file.te \
+	init.te \
+	kickstart.te \
+	mediaserver.te \
+	mpdecision.te \
+	netmgrd.te \
+	qmux.te \
+	rild.te \
+	rmt.te \
+	sensors.te \
+	surfaceflinger.te \
+	system.te \
+	tee.te \
+	thermald.te \
+	ueventd.te \
+	wpa_supplicant.te
