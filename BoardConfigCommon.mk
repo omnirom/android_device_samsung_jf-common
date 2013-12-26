f41fad8bfeeccea14302e5def2452684278663dc
jumoog <kilianvonpflugk@gmail.com>
2013-12-25 19:36:08 -0500

    Fix Wifi Tethering
    Change-Id: I2d00983557ff234087885f2841dcad252065ce74

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,6 +83,17 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/jf-common/bluetoot
 BOARD_BLUEDROID_VENDOR_CONF := device/samsung/jf-common/bluetooth/vnd_jf.txt
 BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 
+# Wifi related defines
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WLAN_DEVICE           := bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_AP      := "/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA     := "etc/wifi/bcmdhd_sta.bin"
+
 # Needed for blobs
 COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 
